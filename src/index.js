cea7d425c5b0885f0a85c761b710aabee373cac7
Hugo Lima <hugo.hnl@gmail.com>
2019-10-03 01:32:40 -0300

    Ajustando Desafio com as correcoes

--- src/index.js
+++ src/index.js
@@ -1,18 +1,14 @@
 const $root = document.querySelector("#root")
 const $memoryCard = document.createElement("article")
-const $memoryCardOpen = document.createElement("article")
-const $bgIconCenter = document.createElement("div")
+const $memoryCardFront = document.createElement("article")
 const $iconGueio = `<img class="icon" src="/images/icon-collabcode.svg" alt="Gueio - Mascote da CollabCode" />`;
 const $iconCPlusPlus = `<img class="icon" src="/images/icon-c.png" alt="C++">`;
 
 $memoryCard.classList.add('memory-card')
-$memoryCardOpen.classList.add('memory-card')
-$memoryCardOpen.classList.add('-open')
-$bgIconCenter.classList.add('bg-center')
-
 $root.insertBefore($memoryCard, null)
 $memoryCard.insertAdjacentHTML('afterbegin', $iconGueio)
 
-$root.insertBefore($memoryCardOpen, $memoryCard)
-$memoryCardOpen.insertBefore($bgIconCenter, null)
-$bgIconCenter.insertAdjacentHTML('afterbegin', $iconCPlusPlus)
+$memoryCardFront.classList.add('memory-card')
+$memoryCardFront.classList.add('-front')
+$root.insertBefore($memoryCardFront, $memoryCard)
+$memoryCardFront.insertAdjacentHTML("afterbegin", $iconCPlusPlus);
