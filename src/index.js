1366e91241dc0e69fc10c0dc7096ef1aebd928d1
Hugo Lima <hugo.hnl@gmail.com>
2019-10-03 00:11:11 -0300

    [021] - Desafio

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 const $root = document.querySelector("#root")
 const $memoryCard = document.createElement("article")
-const $icon = `<img class="icon" src="/images/icon-collabcode.svg" alt="Gueio - Mascote da CollabCode" />`;
+const $memoryCardOpen = document.createElement("article")
+const $bgIconCenter = document.createElement("div")
+const $iconGueio = `<img class="icon" src="/images/icon-collabcode.svg" alt="Gueio - Mascote da CollabCode" />`;
+const $iconCPlusPlus = `<img class="icon" src="/images/icon-c.png" alt="C++">`;
 
-$memoryCard.classList.add("memory-card")
+$memoryCard.classList.add('memory-card')
+$memoryCardOpen.classList.add('memory-card')
+$memoryCardOpen.classList.add('-open')
+$bgIconCenter.classList.add('bg-center')
 
 $root.insertBefore($memoryCard, null)
-$memoryCard.insertAdjacentHTML('afterbegin', $icon)
+$memoryCard.insertAdjacentHTML('afterbegin', $iconGueio)
+
+$root.insertBefore($memoryCardOpen, $memoryCard)
+$memoryCardOpen.insertBefore($bgIconCenter, null)
+$bgIconCenter.insertAdjacentHTML('afterbegin', $iconCPlusPlus)
