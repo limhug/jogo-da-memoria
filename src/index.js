0f67ef8e57ef2a2ea592a43a182e57429627eaba
Hugo Lima <hugo.hnl@gmail.com>
2019-10-04 10:16:09 -0300

    [022] - Desafio Básico

--- src/index.js
+++ src/index.js
@@ -1,14 +1,32 @@
 const $root = document.querySelector("#root")
-const $memoryCard = document.createElement("article")
-const $memoryCardFront = document.createElement("article")
-const $iconGueio = `<img class="icon" src="/images/icon-collabcode.svg" alt="Gueio - Mascote da CollabCode" />`;
-const $iconCPlusPlus = `<img class="icon" src="/images/icon-c.png" alt="C++">`;
 
-$memoryCard.classList.add('memory-card')
-$root.insertBefore($memoryCard, null)
-$memoryCard.insertAdjacentHTML('afterbegin', $iconGueio)
+function memoryCardClose(){
+  const $memoryCard = document.createElement("article")
+  const $iconGueio = `<img class="icon" src="/images/icon-collabcode.svg" alt="Gueio - Mascote da CollabCode" />`;
+  
+  $memoryCard.classList.add('memory-card')
+  $memoryCard.insertAdjacentHTML('afterbegin', $iconGueio)
+  $root.insertBefore($memoryCard, null)
+}
 
-$memoryCardFront.classList.add('memory-card')
-$memoryCardFront.classList.add('-front')
-$root.insertBefore($memoryCardFront, $memoryCard)
-$memoryCardFront.insertAdjacentHTML("afterbegin", $iconCPlusPlus);
+
+function $memoryCardOpen(){  
+  const $memoryCardFront = document.createElement("article")
+  const $iconCPlusPlus = `<img class="icon" src="/images/icon-c.png" alt="C++">`;
+  
+  $memoryCardFront.classList.add('memory-card')
+  $memoryCardFront.classList.add('-front')
+  $memoryCardFront.insertAdjacentHTML("afterbegin", $iconCPlusPlus);
+  $root.insertBefore($memoryCardFront, null)
+}
+
+$memoryCardOpen()
+memoryCardClose()
+memoryCardClose()
+memoryCardClose()
+memoryCardClose()
+memoryCardClose()
+memoryCardClose()
+memoryCardClose()
+memoryCardClose()
+memoryCardClose()
