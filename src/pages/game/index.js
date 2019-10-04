fc329db91380e645a8784d15da1dbc464b59fd3d
Hugo Lima <hugo.hnl@gmail.com>
2019-10-04 15:40:20 -0300

    [025] - Desafio Básico

--- src/pages/game/index.js
+++ src/pages/game/index.js
@@ -1,25 +1,19 @@
 const $root = document.querySelector('#root')
 
 const $cardsWrapper = createCardsWrapper()
-const $card = createMemoryCard(
-  "",
-  "/images/icon-collabcode.svg",
-  "Gueio - mascote da CollabCode"
-)
 
-const $cardFront = createMemoryCard(
-  "-front",
-  "/images/icon-c.png",
-  "C++"
-)
+const $card = createMemoryCard("", "/images/icon-collabcode.svg", "Gueio - mascote da CollabCode")
+const $cardC = createMemoryCard("-front", "/images/icon-c.png", "Ícone de um livro de C++")
+const $cardPHP = createMemoryCard("-front", "/images/icon-php.png", "Ícone de um livro de PHP");
+const $cardJS = createMemoryCard("-front", "/images/icon-js.png", "Ícone de um livro de JavaScript");
 
 $cardsWrapper.insertAdjacentHTML("beforeend", $card)
 $cardsWrapper.insertAdjacentHTML("beforeend", $card)
-$cardsWrapper.insertAdjacentHTML("beforeend", $card)
-$cardsWrapper.insertAdjacentHTML("beforeend", $card)
-$cardsWrapper.insertAdjacentHTML("beforeend", $card)
-$cardsWrapper.insertAdjacentHTML("beforeend", $card)
-$cardsWrapper.insertAdjacentHTML("beforeend", $cardFront)
-$cardsWrapper.insertAdjacentHTML("beforeend", $cardFront)
+$cardsWrapper.insertAdjacentHTML("beforeend", $cardC)
+$cardsWrapper.insertAdjacentHTML("beforeend", $cardC)
+$cardsWrapper.insertAdjacentHTML("beforeend", $cardPHP)
+$cardsWrapper.insertAdjacentHTML("beforeend", $cardPHP)
+$cardsWrapper.insertAdjacentHTML("beforeend", $cardJS)
+$cardsWrapper.insertAdjacentHTML("beforeend", $cardJS)
 
 $root.insertAdjacentElement("beforeend", $cardsWrapper)
